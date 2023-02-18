8b04430d984820880fdc9b62083b0cb8556e98d1
cr0un <vvmanikhin@gmail.com>
2023-02-18 23:37:39 +0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Добавляем константы в файл constants.py
+PWD_HASH_SALT = b'secret here'
+PWD_HASH_ITERATIONS = 100_000
+
+JWT_SECRET_KEY = "V2dsd%$@dasdf"
+JWT_ALGO = "HS256"
---
+++ views/auth.py
@@ -0,0 +1,26 @@
+from flask import request
+from flask_restx import Resource, Namespace
+
+from implemented import auth_service
+
+auth_ns = Namespace("auth")
+
+@auth_ns.route("/")
+class AuthsView(Resource):
+    def post(self):
+        req_json = request.json
+        username = req_json.get("username", None)
+        password = req_json.get("password", None)
+
+        if None in [username, password]:
+            return "", 401
+
+        tokens = auth_service.generate_token(username, password)
+        return tokens, 201
+
+    def put(self):
+        req_json = request.json
+        token = req_json.get("refresh_token")
+        tokens = auth_service.check_token(token)
+
+        return tokens, 201
